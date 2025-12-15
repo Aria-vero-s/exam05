ca6577407c67836686b1a325b9dc839bb35e5c7f
Ariane Saulnier <asaulnie@f0r7s5.clusters.42paris.fr>
2025-12-15 19:40:05 +0100

    sleepy

---
+++ lvl1/bigint/main.cpp
@@ -0,0 +1,51 @@
+#include "bigint.hpp"
+
+// clear && c++ -g main.cpp bigint.cpp bigint.hpp && valgrind --leak-check=full --show-leak-kinds=all ./a.out
+int	main(void)
+{
+	{
+		bigint	a("42234232323423423423423423423443442342334");
+		bigint			b("22342342342342342423423434334333333343434343434343434341"), c, d("1234234234234234234234337"), e(d);
+		
+		a = b;
+		//All are in base10
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << c << std::endl;
+		std::cout << d << std::endl;
+		std::cout << e << std::endl;
+		std::cout << "a + b = " << a + b << std::endl;
+		std::cout << "(c += a) = " << (c += a) << std::endl;
+		
+		std::cout << "\n\n" << std::endl;
+		std::cout << "b = " << b << std::endl;
+		std::cout << "++b = " << ++b << std::endl;
+		std::cout << "b++ = " << b++ << std::endl;
+		std::cout << "b = " << b << std::endl;
+		int	i = 0;
+		std::cout << "i = " << i << std::endl;
+		std::cout << "++i = " << ++i << std::endl;
+		std::cout << "i++ = " << i++ << std::endl;
+		std::cout << "i = " << i << std::endl;
+
+		std::cout << "\n\n" << std::endl;
+		std::cout << "\"((b << 10) + 42)\" = " << ((b << 10) + 42) << std::endl;
+		std::cout << "(d <<= 4) = " << (d <<= 4) << std::endl;
+		std::cout << "(d >>= 2) = " << (d >>= 2) << std::endl;
+		std::cout << "a = " << a << std::endl;
+		std::cout << "d = " << d << std::endl;
+		std::cout << "(d < a) = " << (d < a) << std::endl;
+		std::cout << "(d > a) = " << (d > a) << std::endl;
+		std::cout << "(d == a) = " << (d == a) << std::endl;
+		std::cout << "(d != a) = " << (d != a) << std::endl;
+		std::cout << "(d == a) = " << (d == d) << std::endl;
+		std::cout << "(d <= a) = " << (d <= a) << std::endl;
+		std::cout << "(d >= a) = " << (d >= a) << std::endl;
+
+		bigint x(1234);
+		std::cout << std::endl;
+		std::cout << "\"(x << 2)\"--> " << (x << 2) << "\n";
+		std::cout << "\"(x >>= 2)\"--> " << (x >>= 2) << "\n";
+	}
+	return (0);
+} 
